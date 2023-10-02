51f0e4eeff5af8ab3d47ed8cf45fc3e65c8db5de
evgenyf <evgenyf@EVGENYFEDORED6F>
2023-10-02 16:41:02 +0700

    Добавьте файлы проекта.

---
+++ libnmcan_test_app/Program.cs
@@ -0,0 +1,5 @@
+﻿// See https://aka.ms/new-console-template for more information
+using libnmcan_test_app;
+
+Console.WriteLine("Hello, World!");
+Console.WriteLine(LibnmcanWrapper.CanEnumerator());
